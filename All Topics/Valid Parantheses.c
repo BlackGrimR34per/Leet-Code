80a9031f5fc513a1b537a7ee4372465a3d9ec7dc
Yohen Sheraun <black@Yohens-MacBook-Air.local>
2026-02-15 20:45:49 +0800

    Completed Valid Parantheses

--- All Topics/Valid Parantheses.c
+++ All Topics/Valid Parantheses.c
@@ -1,32 +1,65 @@
 #include <stdbool.h>
+#include <stdio.h>
+
+size_t	ft_strlen(char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+bool	is_open_paranthesis(char c)
+{
+	if (c == '(' || c == '{' || c == '[')
+		return (true);
+	return (false);
+}
+
+bool	checkMatch(char brack_open, char brack_close)
+{
+	if (brack_open == '(' && brack_close == ')')
+		return (true);
+	else if (brack_open == '[' && brack_close == ']')
+		return (true);
+	else if (brack_open == '{' && brack_close == '}')
+		return (true);
+	return (false);
+}
 
 bool	isValid(char *s)
 {
-	int	index;
-	int	paranthesis;
-	int	square_bracket;
-	int	curly_bracket;
-
-	index = -1;
-	paranthesis = 0;
-	square_bracket = 0;
-	curly_bracket = 0;
+	int		index = -1;
+	int		strlen = ft_strlen(s);
+	if (strlen % 2 == 1 || strlen == 0)
+		return (false);
+	int		open_brack_index = 0;
+	char	array[strlen / 2];
 	while (s[++index])
 	{
-		if (s[index] == '(')
-			paranthesis++;
-		else if (s[index] == '[')
-			square_bracket++;
-		else if (s[index] == '{')
-			curly_bracket++;
-		else if (s[index] == ')')
-			paranthesis--;
-		else if (s[index] == ']')
-			square_bracket--;
-		else if (s[index] == '}')
-			curly_bracket--;
+		if (is_open_paranthesis(s[index]))
+		{
+			if (open_brack_index >= strlen/2)
+				return (false);
+			array[open_brack_index++] = s[index];
+		}
+		else
+		{
+			--open_brack_index;
+			if (open_brack_index < 0)
+				return (false);
+			if (!(checkMatch(array[open_brack_index], s[index])))
+				return (false);
+		}
 	}
-	if (square_bracket == 0 && paranthesis == 0 && curly_bracket == 0)
-		return (true);
-	return (false);
+	if (open_brack_index != 0)
+		return (false);
+	return (true);
+}
+
+int	main(void)
+{
+	printf("%d", isValid("()"));
 }
